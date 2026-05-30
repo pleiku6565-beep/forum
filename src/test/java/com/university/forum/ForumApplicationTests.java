bf2c0ccfd5af8730bb491bbcb5e98200c60e2d63
Greenbroop <bongden0329@gmail.com>
2026-05-30 14:44:01 +0700

    Initial commit

---
+++ src/test/java/com/university/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
